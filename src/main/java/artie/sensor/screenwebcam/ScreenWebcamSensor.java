34592cea4946d15cbc7525ec59f3742c79f44f50
Luis Eduardo Imbernón <leimbernon@ai-mind.com>
2019-11-04 20:27:00 +0100

    Added the start and the stop methods

--- src/main/java/artie/sensor/screenwebcam/ScreenWebcamSensor.java
+++ src/main/java/artie/sensor/screenwebcam/ScreenWebcamSensor.java
@@ -2,11 +2,13 @@ package artie.sensor.screenwebcam;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import artie.sensor.common.services.ArtieClientSensorImpl;
 import artie.sensor.screenwebcam.enums.ConfigurationEnum;
+import artie.sensor.screenwebcam.services.ScreenService;
 
 @Service
 public class ScreenWebcamSensor extends ArtieClientSensorImpl{
@@ -18,6 +20,15 @@ public class ScreenWebcamSensor extends ArtieClientSensorImpl{
 	private String screenWriteVideoLocal;
 	@Value("${artie.sensor.screenwebcam.screen.filename}")
 	private String screenFileName;
+	@Value("${artie.sensor.screenwebcam.screen.active}")
+	private String screenActive;
+	
+	//Services
+	@Autowired
+	private ScreenService screenService;
+	
+	//Attributes
+	private boolean screenServiceIsActive = false;
 	
 	/**
 	 * About the sensor information
@@ -36,18 +47,26 @@ public class ScreenWebcamSensor extends ArtieClientSensorImpl{
 		this.configuration.putIfAbsent(ConfigurationEnum.SCREEN_FPS.toString(),this.screenFps);
 		this.configuration.putIfAbsent(ConfigurationEnum.SCREEN_WRITE_VIDEO_LOCAL.toString(),this.screenWriteVideoLocal);
 		this.configuration.putIfAbsent(ConfigurationEnum.SCREEN_FILE_NAME.toString(),this.screenFileName);
+		this.configuration.putIfAbsent(ConfigurationEnum.SCREEN_ACTIVE.toString(),this.screenActive);
+		
+		this.screenServiceIsActive = false;
 	}
 	
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
 		
+		//If the screen capture is enabled
+		if(Boolean.parseBoolean(this.configuration.get(ConfigurationEnum.SCREEN_ACTIVE.toString()))){
+			this.screenService.start();
+			this.screenServiceIsActive = true;
+		}
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
-		
+		//If the screen capture is active
+		if(this.screenServiceIsActive){
+			this.screenService.stop();
+		}
 	}
-	//Configuration
 }
--- src/main/java/artie/sensor/screenwebcam/enums/ConfigurationEnum.java
+++ src/main/java/artie/sensor/screenwebcam/enums/ConfigurationEnum.java
@@ -3,5 +3,6 @@ package artie.sensor.screenwebcam.enums;
 public enum ConfigurationEnum {
 	SCREEN_FPS,
 	SCREEN_WRITE_VIDEO_LOCAL,
-	SCREEN_FILE_NAME
+	SCREEN_FILE_NAME,
+	SCREEN_ACTIVE
 }
